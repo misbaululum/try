0856a09fa8cb6fdaa6a90762276675dc84360ee9
misbaululum <misbaul99@gmail.com>
2023-10-08 23:12:49 +0700

    beranda

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,40 +1,35 @@
 <?= $this->extend('partials/base'); ?>
 
 <?= $this->section('content'); ?>
+
 <div class="container mt-5">
     <div class="glass-container p-4">
     
     <div class="container mt-4">
-        <img src="<?= base_url('public/img/Primadaya-Banner.jpg'); ?>" class="img-fluid" alt="Banner">
+        <img src="<?= base_url('public/img/Primadaya-Banner.jpg'); ?>" class="img-fluid rounded" alt="Banner">
     </div>
-
-        <h1 class="text-center">Selamat Datang di Halaman Beranda Primadaya Plastisindo</h1>
-
+    <br>
+        <h2 class="text-center" style="color:#293773;">Selamat Datang di Halaman Beranda Primadaya Plastisindo</h2>
+<br>
+<br>
         <div class="w3-container w3-green">
-  <h1>Website Form Demo</h1>
-  <p>Percobaan pertama</p>
+  <h3 class="text-center">Website Form Demo</h3>
+  <p class="text-center">Percobaan pertama</p>
 </div>
 
-<div class="w3-row-padding">
-  <div class="w3-third">
-    <h2>London</h2>
+<div class="container px-lg-5">
+  <div class="row mx-lg-n5">
+    <div class="col py-3 px-lg-5"> 
+        <h4>London</h4>
     <p>London is the capital city of England.</p>
     <p>It is the most populous city in the United Kingdom,
-    with a metropolitan area of over 13 million inhabitants.</p>
-  </div>
-
-  <div class="w3-third">
-    <h2>Paris</h2>
+    with a metropolitan area of over 13 million inhabitants.</p></div>
+    <div class="col py-3 px-lg-5">
+    <h4>Paris</h4>
     <p>Paris is the capital of France.</p>
     <p>The Paris area is one of the largest population centers in Europe,
     with more than 12 million inhabitants.</p>
-  </div>
-
-  <div class="w3-third">
-    <h2>Tokyo</h2>
-    <p>Tokyo is the capital of Japan.</p>
-    <p>It is the center of the Greater Tokyo Area,
-    and the most populous metropolitan area in the world.</p>
+    </div>
   </div>
 </div>
 
